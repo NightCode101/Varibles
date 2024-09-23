203a7d72b77a9dffa39f2bee9dd152374d41a0d5
NightCode101 <baoitjerialle01@gmail.com>
2024-09-23 22:24:19 +0800

    Upadated Output

--- Program.cs
+++ Program.cs
@@ -6,37 +6,62 @@ namespace Variable
     {
         static void Main(string[] args)
         {
+            // Add color to the text
+            Console.ForegroundColor = ConsoleColor.Green;
+
             // Declare variables to hold user input
             string fn, mn, ln, age, add, status, zodiac;
 
             // Prompt the user to enter their first name
-            Console.WriteLine("Enter Your First Name:");
+            Console.Write("Enter Your First Name: ");
             fn = Console.ReadLine(); // Read and store the first name
 
+            // Clear the console for better look
+            Console.Clear();
+
             // Prompt the user to enter their middle name
-            Console.WriteLine("Enter Your Middle Name:");
+            Console.Write("Enter Your Middle Name: ");
             mn = Console.ReadLine(); // Read and store the middle name
 
+            // Clear the console for better look
+            Console.Clear();
+
             // Prompt the user to enter their last name
-            Console.WriteLine("Enter Your Last Name:");
+            Console.Write("Enter Your Last Name: ");
             ln = Console.ReadLine(); // Read and store the last name
 
+            // Clear the console for better look
+            Console.Clear();
+
             // Prompt the user to enter their age
-            Console.WriteLine("Enter Your Age:");
+            Console.WriteLine("Enter Your Age: ");
             age = Console.ReadLine(); // Read and store the age
 
+            // Clear the console for better look
+            Console.Clear();
+
             // Prompt the user to enter their address
-            Console.WriteLine("Enter Your Address:");
+            Console.Write("Enter Your Address: ");
             add = Console.ReadLine(); // Read and store the address
 
+            // Clear the console for better look
+            Console.Clear();
+
             // Prompt the user to enter their status
-            Console.WriteLine("Enter Your Status:");
+            Console.Write("Enter Your Status: ");
             status = Console.ReadLine(); // Read and store the status
 
+            // Clear the console for better look
+            Console.Clear();
+
             // Prompt the user to enter their zodiac sign
-            Console.WriteLine("Enter Your Zodiac Sign:");
+            Console.Write("Enter Your Zodiac Sign: ");
             zodiac = Console.ReadLine(); // Read and store the zodiac sign
 
+            // Clear the console for better look
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Red;
             // Output the collected information using string interpolation
             Console.WriteLine($"I am {fn} {mn} {ln}. I am {age} years old, I live in {add}. My status is {status} and my zodiac sign is {zodiac}.");
 
